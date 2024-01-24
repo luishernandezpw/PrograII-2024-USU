4a727c5012f907489a07ba92654ff8c98b208618
Luis Hernandez <sarco_luis@hotmail.com>
2024-01-24 08:33:02 -0600

    Se agrego opciones a la calculadora basica, suma, resta, multiplicacion y division, con RadioGroup y RadioButton.
    Tarea:
    1. Exponenciacion
    2. Porcentaje
    3. Factorial
    4. Raiz

--- controlesbasicos/app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
+++ controlesbasicos/app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView tempVal;
+    RadioGroup opt;
     Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +23,29 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View view) {
                     try{
+                        opt = findViewById(R.id.optCalculadora);
+
                         tempVal = findViewById(R.id.txtnum1);
                         double num1 = Double.parseDouble(tempVal.getText().toString());
 
                         tempVal = findViewById(R.id.txtnum2);
                         double num2 = Double.parseDouble(tempVal.getText().toString());
 
-                        double resp = num1 + num2;
-
+                        double resp = 0;
+                        switch (opt.getCheckedRadioButtonId()){
+                            case R.id.optSuma:
+                                resp = num1+num2;
+                                break;
+                            case R.id.optResta:
+                                resp = num1-num2;
+                                break;
+                            case R.id.optMultiplicacion:
+                                resp=num1*num2;
+                                break;
+                            case R.id.optDivision:
+                                resp = num1/num2;
+                                break;
+                        }
                         tempVal = findViewById(R.id.lblrespuesta);
                         tempVal.setText("Suma: " + resp);
                     }catch (Exception e){
