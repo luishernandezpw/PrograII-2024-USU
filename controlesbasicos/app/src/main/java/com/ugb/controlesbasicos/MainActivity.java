ad73365f20d3e795b9aafe239cacb6429c013271
luishernandezpw <sarco_luis@hotmail.com>
2024-01-31 09:58:32 -0600

    Creacion del diseño delconversor de longitud

--- controlesbasicos/app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
+++ controlesbasicos/app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
@@ -8,12 +8,21 @@ import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TabHost;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TabHost tbh;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tbh = findViewById(R.id.tbhConversores);
+        tbh.setup();
+
+        tbh.addTab(tbh.newTabSpec("longitud").setIndicator("LONGITUD").setContent(R.id.tabLongitud));
+        tbh.addTab(tbh.newTabSpec("almacenamiento").setIndicator("ALMACENAMIENTO").setContent(R.id.tabAlmacenamiento));
+        tbh.addTab(tbh.newTabSpec("monedas").setIndicator("MONEDAS").setContent(R.id.tabMonedas));
     }
 }
