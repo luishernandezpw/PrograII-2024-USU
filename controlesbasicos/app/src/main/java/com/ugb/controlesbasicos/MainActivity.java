d76a988df2000914026077fff4aa61bfc02f323f
Luis Hernandez <sarco_luis@hotmail.com>
2024-02-12 12:48:06 -0600

    Creacion de la rama sensor de GPs y se programo el sensor de GPs, falta revisar dicho codigo

--- controlesbasicos/app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
+++ controlesbasicos/app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
@@ -16,7 +16,9 @@ import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -27,32 +29,40 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     TextView tempVal;
     LocationManager locationManager;
+    LocationListener locationListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         tempVal = findViewById(R.id.lblGps);
-        obtenerPosicion();
+        obtenerUbicacion();
     }
-    private void obtenerPosicion(){
-        try {
-            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                    ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-                tempVal.setText("No tienes permiso de acceder al GPS");
-                return;
+    private void obtenerUbicacion(){
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+            tempVal.setText("NO tienes permiso de acceder al GPS");
+            return;
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_BACKGROUND_LOCATION}, 1);
             }
-            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            mostrarPosicion(location);
-
-        }catch (Exception ex){
-            tempVal.setText(ex.getMessage());
         }
+        /*Location location = new Location(LocationManager.GPS_PROVIDER);
+        mostrarUbicacion(location);*/
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                Log.d("LocationUpdate", "Nueva ubicación: " + location.getLatitude() + ", " + location.getLongitude());
+                mostrarUbicacion(location);
+            }
+        };
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
     }
-    private void mostrarPosicion(Location location){
-        tempVal.setText("Posicion: Latitud: "+ location.getAltitude()+"; Longitud: "+ location.getLongitude() +"; Altitud: "+location.getAltitude());
+    private void mostrarUbicacion(Location location){
+        tempVal.setText("Ubicacion: Altitud: "+ location.getAltitude() +"; Latitud: "+ location.getLatitude() + "; Longitud: "+ location.getLongitude());
     }
-
 }
