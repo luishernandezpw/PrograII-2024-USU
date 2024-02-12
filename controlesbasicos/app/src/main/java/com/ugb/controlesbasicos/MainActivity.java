92fa985832d8143da3d808d1831c0ea65cf6031b
Luis Hernandez <sarco_luis@hotmail.com>
2024-02-12 07:33:45 -0600

    Creacion de la rama sensor Acelerometro y se programo el sensor acelerometro

--- controlesbasicos/app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
+++ controlesbasicos/app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
@@ -2,6 +2,10 @@ package com.ugb.controlesbasicos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,65 +16,50 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView tempVal;
-    RadioGroup opt;
-    Button btn;
-    Spinner spn;
+    SensorManager sensorManager;
+    Sensor sensor;
+    SensorEventListener sensorEventListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-            btn = findViewById(R.id.btnCalcular);
-            btn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    try{
-                        opt = findViewById(R.id.optCalculadora);
-                        spn = findViewById(R.id.spnOpciones);
+        tempVal = findViewById(R.id.lblAcelerometro);
+        activarSensorAcelerometro();
+    }
+    @Override
+    protected void onResume() {
+        iniciarAclerometro();
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        detenerAcelerometro();
+        super.onPause();
+    }
+    private void activarSensorAcelerometro(){
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if(sensor==null){
+            tempVal.setText("Tu telefono NO tiene sensor Acelerometro.");
+            finish();
+        }
+        sensorEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                tempVal.setText("Acelerometro: X="+ sensorEvent.values[0] +"; Y="+ sensorEvent.values[1] +"; Z="+ sensorEvent.values[2]);
+            }
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
 
-                        tempVal = findViewById(R.id.txtnum1);
-                        double num1 = Double.parseDouble(tempVal.getText().toString());
-
-                        tempVal = findViewById(R.id.txtnum2);
-                        double num2 = Double.parseDouble(tempVal.getText().toString());
-
-                        double resp = 0;
-                        /*switch (opt.getCheckedRadioButtonId()){
-                            case R.id.optSuma:
-                                resp = num1+num2;
-                                break;
-                            case R.id.optResta:
-                                resp = num1-num2;
-                                break;
-                            case R.id.optMultiplicacion:
-                                resp=num1*num2;
-                                break;
-                            case R.id.optDivision:
-                                resp = num1/num2;
-                                break;
-                        }*/
-                        switch (spn.getSelectedItemPosition()){
-                            case 0:
-                                resp = num1+num2;
-                                break;
-                            case 1:
-                                resp = num1-num2;
-                                break;
-                            case 2:
-                                resp = num1*num2;
-                                break;
-                            case 3:
-                                resp = num1/num2;
-                                break;
-                        }
-                        tempVal = findViewById(R.id.lblrespuesta);
-                        tempVal.setText("Suma: " + resp );
-                    }catch (Exception e){
-                        tempVal = findViewById(R.id.lblrespuesta);
-                        tempVal.setText("Error: "+ e.getMessage());
-                    }
-                }
-            });
-
+            }
+        };
+    }
+    private void iniciarAclerometro(){
+        sensorManager.registerListener(sensorEventListener, sensor, 2000*1000);
+    }
+    private void detenerAcelerometro(){
+        sensorManager.unregisterListener(sensorEventListener);
     }
 }
