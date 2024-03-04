abb9157069ce946834fb8ea483b029ad0cb8f47a
Luis Hernandez <sarco_luis@hotmail.com>
2024-03-04 07:21:00 -0600

    Programacion del boton de amigos.

--- controlesbasicos/app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
+++ controlesbasicos/app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
@@ -9,12 +9,45 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tempVal;
+    Button btn;
+    String accion="nuevo", id="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btn = findViewById(R.id.btnGuardarAmigos);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tempVal = findViewById(R.id.txtnombre);
+                String nombre = tempVal.getText().toString();
+
+                tempVal = findViewById(R.id.txtdireccion);
+                String direccion = tempVal.getText().toString();
+
+                tempVal = findViewById(R.id.txtTelefono);
+                String tel = tempVal.getText().toString();
+
+                tempVal = findViewById(R.id.txtemail);
+                String email = tempVal.getText().toString();
+
+                tempVal = findViewById(R.id.txtdui);
+                String dui = tempVal.getText().toString();
+
+                DB db = new DB(getApplicationContext(),"", null, 1);
+                String[] datos = new String[]{id,nombre,direccion,tel,email,dui};
+                String respuesta = db.administrar_amigos(accion, datos);
+                if( respuesta.equals("ok") ){
+                    Toast.makeText(getApplicationContext(), "Amigo Registrado con Exito.", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Error: "+ respuesta, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
