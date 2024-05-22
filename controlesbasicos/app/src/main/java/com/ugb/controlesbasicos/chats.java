1a35e3068921b809239dedbff967ea7715c200a4
luishernandezpw <sarco_luis@hotmail.com>
2024-05-22 10:22:42 -0600

    Creacion de la estructura y diseño de los chat

---
+++ controlesbasicos/app/src/main/java/com/ugb/controlesbasicos/chats.java
@@ -0,0 +1,14 @@
+package com.ugb.controlesbasicos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class chats extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chats);
+    }
+}
